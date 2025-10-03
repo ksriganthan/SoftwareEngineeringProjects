3ed24ee4f8f456a3b959ed26ddc7a49d6106016c
Kapischan Sriganthan <kapischan.sriganthan@outlook.com>
2025-10-03 15:50:32 +0200

    Nodes&Edges implementiert - Phase1 beendet.

---
+++ src/main/java/org/NavigationProject/MapData/MapData.java
@@ -0,0 +1,75 @@
+package org.NavigationProject.MapData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapData {
+	private static final String EdgeFile = "/org/NavigationProject/material/edges.txt";
+	private static final String NodeFile = "/org/NavigationProject/material/nodes.txt";
+	private static final Map<String, ArrayList<Destination>> adjacencyList = new HashMap<>();
+	private static final Map<String, GPS> nodes = new HashMap<>();
+
+	public record GPS(Double east, Double north) {
+	};
+
+	public record Destination(String node, double distance) {
+	};
+
+	public MapData() throws Exception {
+		createNodes();
+		createAdjacencyList();
+	}
+
+	public Map<String, GPS> getNodes() {
+		return nodes;
+	}
+
+	public Map<String, ArrayList<Destination>> getAdjacencyList() {
+		return adjacencyList;
+	}
+
+	private void createNodes() throws Exception {
+
+		try (InputStream in = getClass().getResourceAsStream(NodeFile);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+			reader.lines().map(line -> line.split(";"))
+					.forEach(a -> nodes.put(a[0], new GPS(Double.parseDouble(a[1]), Double.parseDouble(a[2]))));
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+	private void createAdjacencyList() throws Exception {
+
+		try (InputStream in = getClass().getResourceAsStream(EdgeFile);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+			reader.lines().map(String::trim) // Leerzeichen weg
+					.filter(line -> !line.isEmpty()) // Leere Zeilen überspringen
+					.map(line -> line.split(";")).forEach(a -> {
+						addDestination(a[0], a[1], a[2]);
+						addDestination(a[1], a[0], a[2]);
+					});
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+	private void addDestination(String startNode, String endNode, String distance) {
+		ArrayList<Destination> destinations = adjacencyList.get(startNode);
+
+		if (destinations == null) {
+			destinations = new ArrayList<Destination>();
+			adjacencyList.put(startNode, destinations);
+		}
+		destinations.add(new Destination(endNode, Double.parseDouble(distance)));
+
+	}
+
+}
---
+++ src/main/java/org/NavigationProject/MapData/Test.java
@@ -0,0 +1,45 @@
+package org.NavigationProject.MapData;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		MapData mapData = new MapData();
+		
+		Map<String, MapData.GPS> nodes = mapData.getNodes();
+		int sumNode = 0;
+		System.out.println("Alle Nodes mit den Koordinaten: \n".toUpperCase());
+		for (String node : nodes.keySet()) {
+			MapData.GPS gps = nodes.get(node);
+			sumNode++;
+			System.out.println(node + ": (" + gps.north() + "," + gps.east() + ")");
+		}
+		System.out.println("==========================================================");
+		System.out.println("ANZAHL NODES: " + sumNode);
+		System.out.println("==========================================================");
+		System.out.println();
+		System.out.println();
+		
+		
+		Map<String, ArrayList<MapData.Destination>>edges = mapData.getAdjacencyList();
+		
+		int sumEdges = 0;
+		System.out.println("Alle Edges mit den Distanzen: \n".toUpperCase());
+		for(String node: edges.keySet()) {
+			for(MapData.Destination neighbour : edges.get(node)) {
+				System.out.println(node + " -> " + neighbour.node() + " (" + neighbour.distance() + ")");
+				sumEdges++;
+			}
+		}
+		System.out.println("==========================================================");
+		System.out.println("ANZAHL Edges: " + sumEdges);
+		System.out.println("==========================================================");
+		System.out.println();
+		System.out.println();
+
+
+	}
+
+}
